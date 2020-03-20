91159dc68b76623767397519a6fab0cb11b8fc30
My Name <my-name@chromium.org>
2020-03-20 21:20:03 +0800

    update

--- securesocketclient/securesocketclient/securesocketclient.cpp
+++ securesocketclient/securesocketclient/securesocketclient.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <QtNetwork/QSslSocket>
+#include <QtNetwork/QSslCipher>
 #include "securesocketclient.h"
 
 securesocketclient::securesocketclient(QWidget *parent)
@@ -12,7 +13,7 @@ securesocketclient::securesocketclient(QWidget *parent)
 
 	connect(ui.hostNameEdit, &QLineEdit::textChanged, this, &securesocketclient::updateEnabledState);
 	connect(ui.connectButton, &QPushButton::clicked, this, &securesocketclient::secureConnect);
-	connect(ui.pushButton_2, &QPushButton::click, this, &securesocketclient::sendData);
+	connect(ui.sendButton, &QPushButton::click, this, &securesocketclient::sendData);
 
 	padLock = new QToolButton;
 	padLock->setIcon(QIcon(":/encrypted.png"));
@@ -41,27 +42,62 @@ securesocketclient::securesocketclient(QWidget *parent)
 
 void securesocketclient::updateEnabledState()
 {
+	const bool unconnected = socket->state() == QAbstractSocket::UnconnectedState;
+	ui.hostNameEdit->setReadOnly(!unconnected);
+	ui.hostNameEdit->setFocusPolicy(unconnected ? Qt::StrongFocus : Qt::NoFocus);
+	ui.hostNameLabel->setEnabled(unconnected);
+	ui.portBox->setEnabled(unconnected);
+	ui.portLabel->setEnabled(unconnected);
+	ui.connectButton->setEnabled(unconnected && !ui.hostNameEdit->text().isEmpty());
 
+	const bool connected = socket->state() == QAbstractSocket::ConnectedState;
+	ui.sessionInput->setEnabled(connected);
+	ui.sessionOutput->setEnabled(connected);
+	ui.sessionInputLabel->setEnabled(connected);
+	ui.connectButton->setEnabled(connected);
 }
 
 void securesocketclient::secureConnect()
 {
-
+	socket->connectToHostEncrypted(ui.hostNameEdit->text(), ui.portBox->value());
+	updateEnabledState();
 }
 
 void securesocketclient::socketStateChanged(QAbstractSocket::SocketState state)
 {
+	if (executingDialog)
+		return;
 
+	updateEnabledState();
+
+	if (state == QAbstractSocket::UnconnectedState) {
+		ui.sessionInput->clear();
+		ui.hostNameEdit->setPalette(QPalette());
+		ui.hostNameEdit->setFocus();
+		ui.cipherLabel->setText("<none>");
+		padLock->hide();
+	}
 }
 
 void securesocketclient::socketEncrypted()
 {
+	ui.sessionOutput->clear();
+	ui.sessionInput->setFocus();
 
+	QPalette palette;
+	palette.setColor(QPalette::Base, QColor(255, 255, 192));
+	ui.hostNameEdit->setPalette(palette);
+
+	const QSslCipher cipher = socket->sessionCipher();
+	const QString cipherInfo = QString("%1, %2 (%3/%4)").arg(cipher.authenticationMethod()).arg(cipher.name()).arg(cipher.usedBits()).arg(cipher.supportedBits());
+
+	ui.cipherLabel->setText(cipherInfo);
+	padLock->show();
 }
 
 void securesocketclient::socketReadyRead()
 {
-
+	appendString(QString::fromUtf8(socket->readAll()));
 }
 
 void securesocketclient::sendData()
@@ -94,4 +130,12 @@ void securesocketclient::setupSecureSocket()
 	connect(socket, qOverload<QAbstractSocket::SocketError>(&QSslSocket::error), this, &securesocketclient::socketError);
 	connect(socket, qOverload<const QList<QSslError>&>(&QSslSocket::sslErrors), this, &securesocketclient::sslErrors);
 	connect(socket, &QSslSocket::readyRead, this, &securesocketclient::socketReadyRead);
+}
+
+void securesocketclient::appendString(const QString& line)
+{
+	QTextCursor cursor(ui.sessionOutput->textCursor());
+	cursor.movePosition(QTextCursor::End);
+	cursor.insertText(line);
+	ui.sessionOutput->verticalScrollBar()->setValue(ui.sessionOutput->verticalScrollBar()->maximum());
 }
